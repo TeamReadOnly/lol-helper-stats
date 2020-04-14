31469819763cfa346939d0a71057745af5c7efe0
Grzegorz Sztuczyński <sztuczynski.grzegorz@gmail.com>
2020-04-14 20:36:53 +0200

    Add sample login screen

---
+++ lib/src/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'widgets/login_screen.dart';
+import 'blocks/provider.dart';
+
+class App extends StatelessWidget {
+  const App({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Provider(
+        child: MaterialApp(
+            home: Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: LoginScreen(),
+    )));
+  }
+}
